3cd7d78f645164ac8dc4a92c7d1f756c0c11dd72
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2025-03-20 01:20:08 +0300

    Add work experience localization

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,9 +1,12 @@
-import { defineI18nConfig } from '@nuxtjs/i18n/dist/runtime/composables';
+import type { I18nOptions } from 'vue-i18n';
 
 import en from './locales/en.json';
 import ru from './locales/ru.json';
 
-export default defineI18nConfig(() => ({
+type MessageSchema = typeof en;
+type Config = I18nOptions<{ message: MessageSchema }, 'en' | 'ru'>;
+
+export default defineI18nConfig<Config>(() => ({
   legacy: false,
   locale: 'en',
   messages: {
